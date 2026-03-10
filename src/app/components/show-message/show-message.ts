261a3a67d294c3f87ba5f35c303ef270b5fb7485
Karol Levi <karollevitafollasalazar@gmail.com>
2026-03-10 12:48:05 -0600

    No se que hacer

--- src/app/components/show-message/show-message.ts
+++ src/app/components/show-message/show-message.ts
@@ -4,7 +4,6 @@ import { MessageServices } from '../../services/message-services';
 
 @Component({
   selector: 'app-show-message',
-  // Ya no necesitas importar CommonModule si usamos la nueva sintaxis
   templateUrl: './show-message.html',
   styleUrl: './show-message.css'
 })
@@ -15,16 +14,13 @@ export class ShowMessage implements OnInit {
   error = '';
 
   ngOnInit(): void {
-    console.log('1. Iniciando petición al backend...');
-    
     this.messageService.listarMensajes().subscribe({
       next: (response) => {
-        console.log('2. ¡Éxito! Datos recibidos:', response);
-        this.messages = response;
+        this.messages = response; 
       },
       error: (err) => {
-        console.error('2. Error en la petición:', err);
-        this.error = 'No se pudieron cargar los mensajes. Revisa la consola (F12).';
+        this.error = 'No se pudieron cargar los mensajes.';
+        console.error(err);
       },
     });
   }
